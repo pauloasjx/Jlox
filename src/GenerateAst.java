040e2e69b34577a0014f8719f3b05affacd9dfef
Paulo Alves <pauloalvsjr@gmail.com>
2022-03-27 19:10:58 -0300

    generate and expr

--- src/GenerateAst.java
+++ src/GenerateAst.java
@@ -19,11 +19,28 @@ public class GenerateAst {
         writer.println("    }");
 
         writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("      return visitor.visit" + className + baseName + "(this);");
+        writer.println("    }");
+
+        writer.println();
         for (String field : fields) {
             writer.println("    final " + field + ";");
         }
 
         writer.println("    }");
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("  interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("  }");
     }
 
     private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
@@ -34,12 +51,17 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         // AST classes
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
+
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
 
         writer.println("}");
         writer.println();
